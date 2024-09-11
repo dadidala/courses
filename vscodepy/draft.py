8dffb6ade9efafbbf4441aec2fb30a72e0fc551b
dadidala <127580842+dadidala@users.noreply.github.com>
2024-09-11 17:44:28 +0800

    Add files via upload

---
+++ vscodepy/draft.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import math
+
+
+# a = pd.read_excel(r"C:\Users\周俊杰\Desktop\SXJM\A题\附件\c1.xlsx")
+# print(a.iloc[:,4])
+# import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+
+plt.plot(x, y)
+plt.xticks(rotation=45)  # 旋转x轴的刻度标签45度
+plt.show()
